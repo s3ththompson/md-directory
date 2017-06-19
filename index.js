b4b49b0febd71868c2a7fe283b5bcada4ec89472
Seth Thompson <s3th.thompson@gmail.com>
2017-06-19 17:16:02 +0200

    .

---
+++ index.js
@@ -0,0 +1,138 @@
+const read = require('read-directory');
+var defaults = require('defaults');
+var compose = require('lodash/flow');
+var commonmark = require('commonmark');
+var matter = require('gray-matter');
+
+var reader = new commonmark.Parser();
+var writer = new commonmark.HtmlRenderer();
+
+function markdown (str) {
+  var parsed = reader.parse(str);
+  return writer.render(parsed);
+}
+
+var mdOptions = {
+  md: markdown,
+  frontmatter: matter,
+  filter: '**/*.md',
+  encoding: 'utf8'
+}
+
+function parseContent(markdown) {
+  return function (obj) {
+    obj.content = markdown(obj.content);
+    return obj
+  }
+}
+
+/**
+* Read the contents of a directory and convert to Markdown asynchronously
+* @name parseDir
+* @param {String} dir – The directory to read
+* @param {Object} options
+* @param {Function} options.md - alternate function to parse markdown, default: commonmark 
+* @param {Function} options.frontmatter - alternate function to parse frontmatter, default: gray-matter
+* @param {String} options.encoding – encoding of files, default: utf8
+* @param {String} options.filter – glob pattern for filtering files, default: `**\/*.md`
+* @param {String} options.ignore – glob pattern for ignoring files
+* @param {Array} options.ignore – array of glob patterns for ignoring files
+* @param {Function} options.transform – A function you can use to transform the contents of files after they are converted
+* @example
+* var md = require('md-directory')
+* md.parseDir('./posts', function (err, contents) {
+*   console.log(contents)
+* })
+**/
+module.exports.parseDir = function parseDir (dir, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  opts = defaults(opts, mdOptions)
+  var transforms = [opts.frontmatter, parseContent(opts.md)];
+  if (opts.transform) transforms.push(opts.transform);
+  opts.transform = compose(transforms);
+  opts.extensions = true;
+  opts.dirnames = true;
+  read(dir, opts, cb);
+}
+
+/**
+* Read the contents of a directory and convert to Markdown synchronously
+* @name parseDirSync
+* @param {String} dir – The directory to read
+* @param {Object} options
+* @param {Function} options.md - alternate function to parse markdown, default: commonmark 
+* @param {Function} options.frontmatter - alternate function to parse frontmatter, default: gray-matter
+* @param {String} options.encoding – encoding of files, default: utf8
+* @param {String} options.filter – glob pattern for filtering files, default: `**\/*.md`
+* @param {String} options.ignore – glob pattern for ignoring files
+* @param {Array} options.ignore – array of glob patterns for ignoring files
+* @param {Function} options.transform – A function you can use to transform the contents of files after they are converted
+* @example
+* var md = require('md-directory')
+* var contents = md.parseDirSync('./posts')
+**/
+module.exports.parseDirSync = function parseDirSync (dir, opts) {
+  opts = defaults(opts, mdOptions)
+  var transforms = [opts.frontmatter, parseContent(opts.md)];
+  if (opts.transform) transforms.push(opts.transform);
+  opts.transform = compose(transforms);
+  opts.extensions = true;
+  opts.dirnames = true;
+  return read.sync(dir, opts);
+}
+
+/**
+* Read the contents of a file and convert to Markdown asynchronously
+* @name parse
+* @param {String} filename – The filename to read
+* @param {Object} options
+* @param {Function} options.md - alternate function to parse markdown, default: commonmark 
+* @param {Function} options.frontmatter - alternate function to parse frontmatter, default: gray-matter
+* @param {String} options.encoding – encoding of files, default: utf8
+* @param {Function} options.transform – A function you can use to transform the contents of files after they are converted
+* @example
+* var md = require('md-directory')
+* md.parse('./post.md', function (err, contents) {
+*   console.log(contents)
+* })
+**/
+module.exports.parse = function parse (filename, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  opts = defaults(opts, mdOptions);
+  var transforms = [opts.frontmatter, parseContent(opts.md)];
+  if (opts.transform) transforms.push(opts.transform);
+  opts.transform = compose(transforms);
+  fs.readFile(filename, {encoding: opts.encoding}, function (err, data) {
+    if (err) return cb(err);
+    var data = opts.transform(data);
+    cb(null, data);
+  });
+}
+
+/**
+* Read the contents of a file and convert to Markdown synchronously
+* @name parseSync
+* @param {String} filename – The filename to read
+* @param {Object} options
+* @param {Function} options.md - alternate function to parse markdown, default: commonmark 
+* @param {Function} options.frontmatter - alternate function to parse frontmatter, default: gray-matter
+* @param {String} options.encoding – encoding of files, default: utf8
+* @param {Function} options.transform – A function you can use to transform the contents of files after they are converted
+* @example
+* var md = require('md-directory')
+* var contents = md.parseSync('./post.md')
+**/
+module.exports.parseSync = function parseSync (filename, opts) {
+  opts = defaults(opts, mdOptions);
+  var transforms = [opts.frontmatter, parseContent(opts.md)];
+  if (opts.transform) transforms.push(opts.transform);
+  opts.transform = compose(transforms);
+  var data = fs.readFileSync(filename, {encoding: opts.encoding});
+  return opts.transform(data)
+}
---
+++ transform.js
@@ -0,0 +1,61 @@
+var path = require('path')
+var through = require('through2')
+var staticModule = require('static-module')
+var md = require('.')
+
+module.exports = function mdDirectoryTransform (filename) {
+  if (/\.json$/.test(filename)) return through()
+
+  var basedir = path.dirname(filename)
+  var vars = {
+    __filename: filename,
+    __dirname: basedir
+  }
+
+  var sm = staticModule({
+    'md-directory': {
+      parseDir: function (dir, opts, cb) {
+        if (typeof opts === 'function') {
+          cb = opts
+          opts = {}
+        }
+        dir = path.isAbsolute(dir) ? dir : path.resolve(dir)
+        var stream = through()
+        stream.push('process.nextTick(function(){(' + cb + ')(null,');
+        stream.push(JSON.stringify(md.parseDirSync(dir, opts)));
+        stream.push(')})');
+        stream.push(null)
+        return stream
+      },
+      parseDirSync: function (dir, opts) {
+        dir = path.isAbsolute(dir) ? dir : path.resolve(dir)
+        var stream = through()
+        stream.push(JSON.stringify(md.parseDirSync(dir, opts)))
+        stream.push(null)
+        return stream
+      },
+      parse: function (filename, opts, cb) {
+        if (typeof opts === 'function') {
+          cb = opts
+          opts = {}
+        }
+        filename = path.isAbsolute(filename) ? filename : path.resolve(filename)
+        var stream = through()
+        stream.push('process.nextTick(function(){(' + cb + ')(null,');
+        stream.push(JSON.stringify(md.parseSync(filename, opts)));
+        stream.push(')})');
+        stream.push(null)
+        return stream
+      },
+      parseSync: function (filename, opts) {
+        filename = path.isAbsolute(filename) ? filename : path.resolve(filename)
+        var stream = through()
+        stream.push(JSON.stringify(md.parseSync(filename, opts)))
+        stream.push(null)
+        return stream
+      }
+    }
+  }, { vars: vars, varModules: { path: path } })
+
+  return sm
+}
